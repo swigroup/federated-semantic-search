539bf8ba49d0e9343a6d9e812ea8de1593c8c1de
Dimitrios Koutsomitropoulos <koutsomi@gmail.com>
2018-08-20 06:16:48 +0300

    - implement XSL transformation to RDF/OWL when producing output in ResultsResource - added dependency on servlet api - kept old XML response in another path ("/results2")

---
+++ SemanticMiddleware/src/main/java/gr/upatras/ceid/hpclab/server/Transform2RDF.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gr.dataverse.duth.semantic.server;
+
+/**
+ *
+ * @author koutsomi
+ */
+import gr.dataverse.duth.semantic.response.model.Results;
+import java.io.OutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletContext;
+import javax.xml.bind.*;
+import javax.xml.bind.util.JAXBSource;
+import javax.xml.transform.*;
+import javax.xml.transform.stream.*;
+
+public  class Transform2RDF {
+
+    private static String realPath;
+    private static TransformerFactory tf;
+    private static StreamSource xslt;
+
+    static void transform(Results r, StreamResult result, ServletContext context)
+      {
+        try {
+            realPath = context.getRealPath("/WEB-INF/classes/2LOM.xsl");
+            xslt = new StreamSource(realPath);
+            
+            tf = TransformerFactory.newInstance();
+            JAXBContext jc = JAXBContext.newInstance(Results.class);
+            JAXBSource source = new JAXBSource(jc, r);
+            Transformer transformer = tf.newTransformer(xslt);
+            transformer.transform(source, result);
+        } catch (JAXBException ex) {
+            Logger.getLogger(Transform2RDF.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (TransformerConfigurationException ex) {
+            Logger.getLogger(Transform2RDF.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (TransformerException ex) {
+            Logger.getLogger(Transform2RDF.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+   
+}
---
+++ SemanticMiddleware/src/main/resources/2LOM.xsl
@@ -0,0 +1,40 @@
+﻿<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0" 
+  xmlns:owl="http://www.w3.org/2002/07/owl#"
+  xmlns:lom="http://ltsc.ieee.org/xsd/LOM/"
+  xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+  xmlns:xsd="http://www.w3.org/2001/XMLSchema#">
+  <xsl:template match="/">
+  <rdf:RDF xmlns="http://lom.duth.gr/"
+     xml:base="http://lom.duth.gr"
+     xmlns:xml="http://www.w3.org/XML/1998/namespace"
+     xmlns:xsd="http://www.w3.org/2001/XMLSchema#"
+     xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+     xmlns:skos="http://www.w3.org/2004/02/skos/core#">
+    <owl:Ontology rdf:about="">
+       
+    </owl:Ontology>
+    
+  <xsl:apply-templates/>
+  </rdf:RDF>
+  </xsl:template>
+  
+  <xsl:template match="Results/Category">
+<xsl:apply-templates select="Result"/>
+  </xsl:template>
+  
+  <xsl:template match="Result">
+<owl:NamedIndividual rdf:ID="{generate-id(.)}">
+<lom:keyword><xsl:value-of select="../@keyword"/></lom:keyword>
+<xsl:apply-templates select="../matching_concept/@IRI"/>
+<lom:identifier rdf:type="xsd:anyURI"><xsl:value-of select="URL"/></lom:identifier>
+<lom:title><xsl:value-of select="Title"/></lom:title>
+<lom:description><xsl:value-of select="Description"/></lom:description>
+</owl:NamedIndividual>
+  </xsl:template>
+  
+  <xsl:template match="matching_concept/@IRI">
+  <lom:keyword rdf:resource="{.}"/> 
+  </xsl:template>
+  
+</xsl:stylesheet>
